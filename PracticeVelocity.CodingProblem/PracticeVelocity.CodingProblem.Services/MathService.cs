a2da642eb8b0e5c971f19fe26b361d4280a64e4d
swapna <swapna.bongunoori86@gmail.com>
2017-02-01 14:44:47 -0600

    Rephrased Erro Message.

--- PracticeVelocity.CodingProblem/PracticeVelocity.CodingProblem.Services/MathService.cs
+++ PracticeVelocity.CodingProblem/PracticeVelocity.CodingProblem.Services/MathService.cs
@@ -12,7 +12,7 @@ namespace PracticeVelocity.CodingProblem.Services
         {
             if (denominator == 0)
             {
-                throw new Exception(@"Cannot devide with zero. Imagine that you have zero cookies, and you split them evenly among zero friends. How many cookies does each person get? See? It doesn’t make sense.");
+                throw new Exception(@"Cannot devide with zero.");
             }
             return (numerator % denominator) == 0 ? true : false;
         }
